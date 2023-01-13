e1bc86a2b0218d6eb5cf4f5d714f35d597a13208
Prathap <prathap.sims@gmail.com>
2023-01-12 19:10:19 -0800

    Initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+const style = {
+  nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
+  heading: `text-white text-3xl`
+}
+const Navbar = () => {
+  return (
+    <div className={style.nav}>
+      <h1 className={style.heading}>Mate</h1>
+
+    </div>
+  )
+}
+
+export default Navbar;
